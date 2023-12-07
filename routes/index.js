279e197951fc4f2850f03e88fc02e4e77f34800e
bbageon <129655962+bbageon@users.noreply.github.com>
2023-12-07 19:31:10 +0900

    mysql 세팅
    mysql 세팅

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../db/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res) => {
   res.render('index', { title: 'Express' });
+  const selectuser = await pool.query("select * from user");
+  console.log(selectuser[0])
 });
 
 module.exports = router;
