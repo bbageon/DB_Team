cf0310a7cec30356c1e4300f2c5a1e2a92abd511
Ojun0 <ojy814@naver.com>
2023-12-15 00:23:49 +0900

    음료 디테일

--- db/session.js
+++ db/session.js
@@ -4,13 +4,9 @@ var options = {
     host: "localhost",
     port: 3306,
     user: "root",
-<<<<<<< Updated upstream
     password: "00000000",
     database: "db_team",
-=======
-    password: "",
-    database: "coffeeshop",
->>>>>>> Stashed changes
+
 };
 var sessionStore = new MySQLStore(options);
 module.exports = sessionStore;
--- routes/community.js
+++ routes/community.js
@@ -3,8 +3,8 @@ var router = express.Router();
 const pool = require('../db/db');
 
 router.get('/', async (req, res) => {
-    const custumDrink = await pool.query("select * from db_team.recipe;")
-    console.log(custumDrink)
+    const custumDrink = await pool.query("select * from db_team.recipe where share_is = 'Y';")
+    console.log(custumDrink[0][0]);
     
     res.render('community', {
         custumDrink: custumDrink[0],
