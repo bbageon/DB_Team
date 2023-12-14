3ab4a2c27dfe3105d1b4b14bb40aaf9b60f762ed
bbageon <rjsdn4403@gmail.com>
2023-12-14 21:37:06 +0900

    asd
    asd

--- routes/custom.js
+++ routes/custom.js
@@ -3,7 +3,31 @@ var router = express.Router();
 const pool = require('../db/db');
 
 router.get('/', async (req, res) => {
-    res.render('custom');
+    try {
+        let base = []
+        let ingredient = []
+        // 재료검색 후, 값 뿌려줌 
+        const selecting = await pool.query("select * from ingredient");
+        for(i = 0; i < selecting[0].length; i++){
+            if (selecting[0][i].type == "base"){
+                base.push(selecting[0][i]);
+            } else {
+                ingredient.push(selecting[0][i]);
+            }
+        }
+        // console.log("!!!", base, ingredient );
+        res.render('custom', { 
+            value1 : base,
+            value2 : ingredient 
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 })
 
+
+router.post('/makedrink', async (req, res) => {
+    console.log(req.body)
+})
 module.exports = router;
