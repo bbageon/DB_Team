74fb6ee4102b14038b3e7ad8eca3909933ed38e7
bbageon <rjsdn4403@gmail.com>
2023-12-15 04:34:47 +0900

    맞춤형 음료 등록
    맞춤형 음료 등록

--- routes/custom.js
+++ routes/custom.js
@@ -28,6 +28,28 @@ router.get('/', async (req, res) => {
 
 
 router.post('/makedrink', async (req, res) => {
-    console.log(req.body)
+    try {
+        let temp = [];
+        let values = Object.values(req.body);
+        // 세션 삽입후 고침
+        const insertrecipe = await pool.query("insert into recipe (user_userid) values (?) ",[1]);
+        // 최근에 삽입된 orderid 삽입
+        const result = await pool.query("SELECT LAST_INSERT_ID() as id");
+        const orderid = result[0][0].id;
+        for (i=0; i < values.length; i++) {
+            // 재료 선택한거만
+            if (values[i] != 0) {
+                // 양이랑 가격 받아오면 수정
+                const recipedetail = await pool.query("insert into recipe_has_ingredient values (?,?,?,?)",
+                [orderid, 1, 1, values[i]]);
+                temp.push(values[i]);
+            }
+        }
+        console.log(temp[0])
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 module.exports = router;
+
