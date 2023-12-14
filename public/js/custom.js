2d9b58acab9007251bdcfde66b20a2b103a22b4d
wjdghks3431 <wjdghks3431@naver.com>
2023-12-14 18:41:20 +0900

    .

--- public/js/custom.js
+++ public/js/custom.js
@@ -63,3 +63,50 @@ function register() {
     // 여기에 등록 로직을 추가할 수 있습니다.
     alert('등록하기 버튼이 클릭되었습니다.');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // 컵사이즈 옵션 변경 이벤트 리스너 등록
+    var cupSizeSelect = document.querySelector("#cupSize");
+    cupSizeSelect.addEventListener("change", function() {
+        // 선택된 컵사이즈의 값 가져오기
+        var selectedCupSize = parseInt(cupSizeSelect.value);
+
+        // 베이스와 재료들의 값을 컵사이즈에 맞게 업데이트
+        updateBaseValue(selectedCupSize);
+        updateIngredientValue(selectedCupSize);
+    });
+
+    // 베이스 값 업데이트 함수
+    function updateBaseValue(selectedCupSize) {
+        // 베이스 옵션들의 부모 요소인 tr 가져오기
+        var baseRow = document.querySelector("#baseRow");
+
+        // 선택된 컵사이즈에 따라 베이스 값 업데이트
+        var baseValue = selectedCupSize * 0.2; // 예시로 0.2를 곱하는데 실제로는 적절한 계산 필요
+        baseRow.querySelector(".base-value").textContent = baseValue + "원";
+
+        // 베이스의 ml 테이블 값 업데이트
+        var mlTable = baseRow.querySelector(".ml-value");
+        mlTable.textContent = selectedCupSize + "ml";
+    }
+
+    // 재료 값 업데이트 함수
+    function updateIngredientValue(selectedCupSize) {
+        // 재료 옵션들의 부모 요소인 tr 가져오기
+        var ingredientRow = document.querySelector("#ingredientRow");
+
+        // 선택된 컵사이즈에 따라 재료 값 업데이트
+        var ingredientValue = selectedCupSize * 0.1; // 예시로 0.1을 곱하는데 실제로는 적절한 계산 필요
+        ingredientRow.querySelector(".ingredient-value").textContent = ingredientValue + "원";
+
+        // 재료의 ml 테이블 값 업데이트
+        var mlTable = ingredientRow.querySelector(".ml-value");
+        mlTable.textContent = selectedCupSize + "ml";
+    }
+});
+
+// 삭제 버튼 클릭 시 해당 행 삭제하는 함수
+function deleteRow(button) {
+    var row = button.closest("tr");
+    row.parentNode.removeChild(row);
+}
